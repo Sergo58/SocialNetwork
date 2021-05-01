a57dc8b1dd22fea06d9e824c3d292c5388a9d06f
Sergo58 <godfather59@yandex.ru>
2021-05-01 23:01:09 +0500

    Updates

---
+++ precache-manifest.4a8fb3f6f5ab317f800687098217352d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2dfab2d6bd99b4e3169ea930f8e13c",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "972041fe8b41fa23f483",
+    "url": "/SocialNetwork/static/css/main.f6b9312c.chunk.css"
+  },
+  {
+    "revision": "ba10503289994673f577",
+    "url": "/SocialNetwork/static/js/2.c44af6eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.c44af6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972041fe8b41fa23f483",
+    "url": "/SocialNetwork/static/js/main.f3ed8c9a.chunk.js"
+  },
+  {
+    "revision": "cbcf78d674966714af32",
+    "url": "/SocialNetwork/static/js/runtime-main.bbb63f0e.js"
+  },
+  {
+    "revision": "5791e18c9eed103e01fded087e68ca2d",
+    "url": "/SocialNetwork/static/media/Infinity-1.7s-200px (2).5791e18c.svg"
+  },
+  {
+    "revision": "13ab99f3472c9fbb5fc74f36be347932",
+    "url": "/SocialNetwork/static/media/userPhoto.13ab99f3.jpg"
+  }
+]);
